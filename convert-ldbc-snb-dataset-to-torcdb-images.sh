db7f473f28af241dcdaaa6665e8a881754642071
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-08-07 20:13:26 -0600

    Added GRAPH_NAME param to image file maker script.
    Now the output will be named with the right graph name.

--- convert-ldbc-snb-dataset-to-torcdb-images.sh
+++ convert-ldbc-snb-dataset-to-torcdb-images.sh
@@ -5,19 +5,21 @@ then
   echo "Take LDBC SNB dataset files distributed across a set of servers and "
   echo "convert them to TorcDB RAMCloud image files in parallel."
   echo ""
-  echo "Usage: convert-ldbc-snb-dataset-to-torcdb-images.sh SERVER_LIST REMOTE_DIR"
+  echo "Usage: convert-ldbc-snb-dataset-to-torcdb-images.sh SERVER_LIST REMOTE_DIR GRAPH_NAME"
   echo "  SERVER_LIST      List of hostnames of the servers that have "
   echo "                   partitions of the dataset, one per line."
   echo "  REMOTE_DIR       Location of the dataset partitions on the remote "
   echo "                   hosts. Expects social_network/ and "
   echo "                   social_network_supplementary_files/ in this "
   echo "                   directory."
+  echo "  GRAPH_NAME       Name of the graph."
 
   exit
 fi
 
 SERVER_LIST=$1
 REMOTE_DIR=$2
+GRAPH_NAME=$3
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
@@ -35,4 +37,4 @@ NUM_SERVERS=${#hosts[@]}
 # Prep maven exec plugin
 mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls .interactive.torc.util.ImageMaker" -Dexec.args="" > /dev/null 2>&1
 
-echo ${hosts[@]} | pdsh -R ssh -w - "cd ${SCRIPTPATH}; mvn exec:java -Dexec.mainClass=\"net.ellitron.ldbcsnbimpls.interactive.torc.util.ImageMaker\" -Dexec.args=\"--mode all --outputDir ${REMOTE_DIR} --noLabelList --graphName graph --numThreads 4 --reportInt 2 --reportFmt LFDT ${REMOTE_DIR}/social_network ${REMOTE_DIR}/social_network_supplementary_files\""
+echo ${hosts[@]} | pdsh -R ssh -w - "cd ${SCRIPTPATH}; mvn exec:java -Dexec.mainClass=\"net.ellitron.ldbcsnbimpls.interactive.torc.util.ImageMaker\" -Dexec.args=\"--mode all --outputDir ${REMOTE_DIR} --noLabelList --graphName ${GRAPH_NAME} --numThreads 4 --reportInt 2 --reportFmt LFDT ${REMOTE_DIR}/social_network ${REMOTE_DIR}/social_network_supplementary_files\""
