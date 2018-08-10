e5abdaae848ef0b41b2f77bd491ad1d2e1467b06
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-08-10 16:11:46 -0600

    Uncommented accidentally commented out code.

--- shuffle-merge-and-compress-ramcloud-tablet-images.sh
+++ shuffle-merge-and-compress-ramcloud-tablet-images.sh
@@ -37,13 +37,13 @@ NUM_SERVERS=${#hosts[@]}
 
 # Prepend hostname to files on each host so that shuffling does not clobber
 # itself.
-#echo ${hosts[@]} | pdsh -R ssh -w - "for file in \$(find ${REMOTE_DIR} -name '*.img'); do mv \${file} \$(dirname \${file})/\$(hostname --short).\$(basename \${file}); done"
+echo ${hosts[@]} | pdsh -R ssh -w - "for file in \$(find ${REMOTE_DIR} -name '*.img'); do mv \${file} \$(dirname \${file})/\$(hostname --short).\$(basename \${file}); done"
 
 # Do the shuffling.
-#echo ${hosts[@]} | pdsh -R ssh -w - "hosts=(${hosts[@]}); for (( tabletNum = 1; ; tabletNum++ )); do if [[ \${hosts[\${tabletNum} - 1]} == \$(hostname --short) ]]; then continue; fi; tablet_listing=\$(find ${REMOTE_DIR} -name \"*tablet\${tabletNum}*.img\"); if [[ -z \"\${tablet_listing}\" ]]; then exit; fi; file_array=(); for file in \${tablet_listing}; do file_array+=(\${file}); done; echo \"Sending tablet \${tabletNum} to \${hosts[\${tabletNum} - 1]}...\"; tar -c \"\${file_array[@]}\" 2>/dev/null | ssh \${hosts[\${tabletNum} - 1]} \"tar -xvf - -C /\" > /dev/null; rm \"\${file_array[@]}\"; done" 
+echo ${hosts[@]} | pdsh -R ssh -w - "hosts=(${hosts[@]}); for (( tabletNum = 1; ; tabletNum++ )); do if [[ \${hosts[\${tabletNum} - 1]} == \$(hostname --short) ]]; then continue; fi; tablet_listing=\$(find ${REMOTE_DIR} -name \"*tablet\${tabletNum}*.img\"); if [[ -z \"\${tablet_listing}\" ]]; then exit; fi; file_array=(); for file in \${tablet_listing}; do file_array+=(\${file}); done; echo \"Sending tablet \${tabletNum} to \${hosts[\${tabletNum} - 1]}...\"; tar -c \"\${file_array[@]}\" 2>/dev/null | ssh \${hosts[\${tabletNum} - 1]} \"tar -xvf - -C /\" > /dev/null; rm \"\${file_array[@]}\"; done" 
 
 # Merge tablet image files
-#echo ${hosts[@]} | pdsh -R ssh -w - "cd ${REMOTE_DIR}; for tableName in \$(ls); do echo \"Merging \${tableName} tablets...\"; for imageFile in \$(ls \${tableName}); do cat \${tableName}/\${imageFile} >> \${tableName}/\${imageFile#*.*.}; rm \${tableName}/\${imageFile}; done; done"
+echo ${hosts[@]} | pdsh -R ssh -w - "cd ${REMOTE_DIR}; for tableName in \$(ls); do echo \"Merging \${tableName} tablets...\"; for imageFile in \$(ls \${tableName}); do cat \${tableName}/\${imageFile} >> \${tableName}/\${imageFile#*.*.}; rm \${tableName}/\${imageFile}; done; done"
 
 # Merge tablet image files
 echo ${hosts[@]} | pdsh -R ssh -w - "cd ${REMOTE_DIR}; for tableName in \$(ls); do for imageFile in \$(ls \${tableName}); do echo \"Compressing \${tableName}/\${imageFile}...\"; gzip \${tableName}/\${imageFile}; done; done"
