90182af46c93fc18660524e2c247ffcc457aef42
Jonathan Ellithorpe <jde@cs.stanford.edu>
2018-10-09 19:12:24 -0600

    Updated social-network-stats.sh to calc # edge lists.

--- social-network-stats.sh
+++ social-network-stats.sh
@@ -38,18 +38,53 @@ echo "  Total: $globalnodecount"
 echo "Edges:"
 
 globaledgecount=0
-for edgetype in comment_hasCreator_person comment_hasTag_tag comment_isLocatedIn_place comment_replyOf_comment comment_replyOf_post forum_containerOf_post forum_hasMember_person forum_hasModerator_person forum_hasTag_tag organisation_isLocatedIn_place person_email_emailaddress person_hasInterest_tag person_isLocatedIn_place person_knows_person person_likes_comment person_likes_post person_speaks_language person_studyAt_organisation person_workAt_organisation place_isPartOf_place post_hasCreator_person post_hasTag_tag post_isLocatedIn_place tagclass_isSubclassOf_tagclass tag_hasType_tagclass
+for edgetype in comment_hasCreator_person comment_hasTag_tag comment_isLocatedIn_place comment_replyOf_comment comment_replyOf_post forum_containerOf_post forum_hasMember_person forum_hasModerator_person forum_hasTag_tag organisation_isLocatedIn_place person_hasInterest_tag person_isLocatedIn_place person_knows_person person_likes_comment person_likes_post person_studyAt_organisation person_workAt_organisation place_isPartOf_place post_hasCreator_person post_hasTag_tag post_isLocatedIn_place tagclass_isSubclassOf_tagclass tag_hasType_tagclass
 do
-  edgetypecount=0
+  edgecount=0
+  edgelistcount_out=0
+  edgelistcount_in=0
+
+  # Count the total number of edges and also the number of out-bound edge lists,
+  # which is the number of source vertices. In the case of person_knows_person
+  # we need to use the dual file in the supplementary directory, handled in the
+  # for loop below.
   for file in $(ls ${DATASET}/social_network/${edgetype}_[0-9]*_0.csv)
   do
-    subcount=$(wc -l ${file} | awk '{print $1}')
-    (( edgetypecount += subcount - 1 ))
+    edgesubcount=$(wc -l ${file} | awk '{print $1}')
+    (( edgecount += edgesubcount - 1 ))
+
+    if [[ ${edgetype} != "person_knows_person" ]]
+    then
+      edgelistsubcount_out=$(cat ${file} | cut -d'|' -f1 | uniq | wc -l)
+      (( edgelistcount_out += edgelistsubcount_out - 1 ))
+    fi
   done
 
-  echo "  $edgetype: $edgetypecount"
+  # Count the total number of in-bound edge lists, which is the total number of 
+  # destination vertices. In the case of person_knows_person, we really have no
+  # in-bound edges, because all the knows edges are actually out-bound in TorcDB
+  # and the dual file contains this information.
+  if [[ ${edgetype} == "person_knows_person" ]]
+  then
+    for file in $(ls ${DATASET}/social_network_supplementary_files/${edgetype}_[0-9]*_0.csv)
+    do
+      edgelistsubcount_out=$(cat ${file} | cut -d'|' -f1 | uniq | wc -l)
+      (( edgelistcount_out += edgelistsubcount_out - 1 ))
+    done
+  else
+    for file in $(ls ${DATASET}/social_network_supplementary_files/${edgetype}_ridx_[0-9]*_0.csv)
+    do
+      edgelistsubcount_in=$(cat ${file} | cut -d'|' -f1 | uniq | wc -l)
+      (( edgelistcount_in += edgelistsubcount_in - 1 ))
+    done
+  fi
 
-  (( globaledgecount += edgetypecount ))
+
+  echo "  $edgetype edge count: $edgecount"
+  echo "  $edgetype OUT edge list count: $edgelistcount_out"
+  echo "  $edgetype IN edge list count: $edgelistcount_in"
+
+  (( globaledgecount += edgecount ))
 
 done
 
