d01444a8a32061261bfab3d8477d2eea2bed6938
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-03-27 12:54:50 -0600

    Added default scripts.

---
+++ distribute-torcdb-image-files-defaults.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./distribute-torcdb-image-files.sh /remote/ldbc-snb/ldbc_snb_sf0100/torcdb_ramcloud_image_files/tablets-8/ 8servers.txt /local/rcbackup
---
+++ upload-distributed-torcdb-image-files-defaults.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb_ramcloud_image_files/tablets-8/ $(rccoordip rc16) ~/RAMCloudUtils/
