10f52f15e429b2694cefa994c1a6b1215bd282be
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-05-03 20:30:24 -0600

    Added TorcDB2 image generation scripts.

---
+++ convert-ldbc-snb-dataset-to-torcdb2-images.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [[ $# != 3 ]]
+then
+  echo "Take LDBC SNB dataset files distributed across a set of servers and "
+  echo "convert them to TorcDB2 RAMCloud image files in parallel."
+  echo ""
+  echo "Usage: convert-ldbc-snb-dataset-to-torcdb2-images.sh SERVER_LIST REMOTE_DIR GRAPH_NAME"
+  echo "  SERVER_LIST      List of hostnames of the servers that have "
+  echo "                   partitions of the dataset, one per line."
+  echo "  REMOTE_DIR       Location of the dataset partitions on the remote "
+  echo "                   hosts. Expects social_network/ and "
+  echo "                   social_network_supplementary_files/ in this "
+  echo "                   directory."
+  echo "  GRAPH_NAME       Name of the graph."
+
+  exit
+fi
+
+SERVER_LIST=$1
+REMOTE_DIR=$2
+GRAPH_NAME=$3
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Read in the server list as an array
+while read -r hostname
+do
+  if [[ ! $hostname =~ ^#.* ]]
+  then
+    hosts+=($hostname)
+  fi
+done < ${SERVER_LIST}
+
+NUM_SERVERS=${#hosts[@]}
+
+# Prep maven exec plugin
+mvn exec:java -Dexec.mainClass="net.ellitron.ldbcsnbimpls.interactive.torcdb2.util.ImageMaker" -Dexec.args="" > /dev/null 2>&1
+
+echo ${hosts[@]} | pdsh -R ssh -w - "mkdir ${REMOTE_DIR}/image_files"
+echo ${hosts[@]} | pdsh -R ssh -w - "cd ${SCRIPTPATH}; mvn exec:java -Dexec.mainClass=\"net.ellitron.ldbcsnbimpls.interactive.torcdb2.util.ImageMaker\" -Dexec.args=\"--mode all --outputDir ${REMOTE_DIR}/image_files --graphName ${GRAPH_NAME} --numThreads 4 --reportInt 2 --reportFmt LFDT ${REMOTE_DIR}/social_network ${REMOTE_DIR}/social_network_supplementary_files\""
+echo ${hosts[@]} | pdsh -R ssh -w - "cd ${REMOTE_DIR}/image_files; for file in \$(ls *.img); do imageName=\${file%.img}; part=\${imageName%%.*}; tableName=\${imageName#\${part}.}; mkdir -p \${tableName}; mv \${file} ./\${tableName}/\${part}.img; done"
---
+++ generate-torcdb2-image-files.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+if [[ $# != 6 ]]
+then
+  echo "Take LDBC SNB dataset files that have already been partitioned "
+  echo "entity-wise into N partitions and distribute those files across a "
+  echo "cluster of N machines and convert the dataset into TorcDB2 images, "
+  echo "divided into N tablets, and copy the files back to a CloudLab dataset."
+  echo ""
+  echo "Usage: generate-torcdb-image-files.sh DATASET_DIR SERVER_LIST REMOTE_DIR GRAPH_NAME RAMCLOUD_UTILS CL_DATASET_DIR"
+  echo "  DATASET_DIR     LDBC SNB dataset directory containing "
+  echo "                  social_network/ and "
+  echo "                  social_network_supplementary_files/ directories "
+  echo "                  divided into N partitions. N is detected "
+  echo "                  automatically from the dataset files."
+  echo "  SERVER_LIST     List of hostnames of the servers to copy the "
+  echo "                  dataset to, one per line. If there are more than N "
+  echo "                  servers, will use the first N."
+  echo "  REMOTE_DIR      Local disk directory on remote host to copy files "
+  echo "                  to."
+  echo "  GRAPH_NAME      Name of the graph."
+  echo "  RAMCLOUD_UTILS  Path to RAMCloudUtils directory. Needed for finding "
+  echo "                  ImageFileHashPartitioner."
+  echo "  CL_DATASET_DIR  Where to store finished dataset in CloudLab storage."
+  echo "                  Will automatically create a directory called "
+  echo "                  tablets-N here."
+
+  exit
+fi
+
+DATASET_DIR=$1
+SERVER_LIST=$2
+REMOTE_DIR=$3
+GRAPH_NAME=$4
+RAMCLOUD_UTILS=$5
+CL_DATASET_DIR=$6
+
+# Read in the server list as an array
+while read -r hostname
+do
+  if [[ ! $hostname =~ ^#.* ]]
+  then
+    hosts+=($hostname)
+  fi
+done < ${SERVER_LIST}
+
+NUM_SERVERS=${#hosts[@]}
+
+N=$(ls ${DATASET_DIR}/social_network/ | grep comment_hasCreator_person | awk -F'_' 'BEGIN{max=0} {if (max < $4) max = $4} END{print max + 1}')
+
+if (( NUM_SERVERS < N ))
+then
+  echo "ERROR: Need ${N} servers but only ${NUM_SERVERS} in ${SERVER_LIST}."
+  exit
+fi
+
+# Create a file that is only the servers that we need
+rm -f ${SERVER_LIST}.tmp
+for (( i = 0; i < N; i++ ))
+do
+  echo ${hosts[$i]} >> ${SERVER_LIST}.tmp
+done
+
+echo "Distributing files to servers' local disk..."
+./distribute-partitioned-ldbc-snb-dataset.sh ${DATASET_DIR} ${N} ${SERVER_LIST}.tmp ${REMOTE_DIR}
+echo 
+
+echo "Converting dataset files to TorcDB2 images..."
+./convert-ldbc-snb-dataset-to-torcdb2-images.sh ${SERVER_LIST}.tmp ${REMOTE_DIR}/${DATASET_DIR} ${GRAPH_NAME}
+echo 
+
+echo "Dividing images into tablets..."
+./partition-torcdb-images-into-ramcloud-tablets.sh ${RAMCLOUD_UTILS} ${SERVER_LIST}.tmp ${REMOTE_DIR}/${DATASET_DIR}/image_files ${N}
+echo 
+
+echo "Merging tablets together and compressing them..."
+./shuffle-merge-and-compress-ramcloud-tablet-images.sh ${SERVER_LIST}.tmp ${REMOTE_DIR}/${DATASET_DIR}/image_files
+echo
+
+echo "Moving compressed tablets back to CloudLab dataset..."
+mkdir -p ${CL_DATASET_DIR}/tablets-${N}
+./copy-compressed-tablets-to-cloudlab-dataset.sh ${SERVER_LIST}.tmp ${REMOTE_DIR}/${DATASET_DIR}/image_files ${CL_DATASET_DIR}/tablets-${N}
+echo
+
+echo "Done!"
+
+rm -f ${SERVER_LIST}.tmp
