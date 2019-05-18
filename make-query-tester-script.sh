16cc6a40dadb0544d20b84cf188fcc97ac45543d
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-05-17 22:46:06 -0600

    Updated make-query-tester-script.sh with usage of --warmUp.

--- make-query-tester-script.sh
+++ make-query-tester-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a query_argfmts
-query_argfmts[1]="\${args[0]} \${args[1]} 20"
+query_argfmts[1]="\${args[0]} \\\"\${args[1]}\\\" 20"
 query_argfmts[2]="\${args[0]} \${args[1]} 20"
 query_argfmts[3]="\${args[0]} \${args[3]} \${args[4]} \${args[1]} \${args[2]} 20"
 query_argfmts[4]="\${args[0]} \${args[1]} \${args[2]} 10"
@@ -29,7 +29,13 @@ execQuery() {
     then
       continue
     fi
-    eval "echo \"query${number} ${query_argfmts[number]} --repeat ${repeat} --timeUnits=MICROSECONDS\""
+
+    if (( param_set_nr < 4 ))
+    then
+      eval "echo \"query${number} ${query_argfmts[number]} --warmUp 60 --repeat ${repeat} --timeUnits=MICROSECONDS\""
+    else
+      eval "echo \"query${number} ${query_argfmts[number]} --repeat ${repeat} --timeUnits=MICROSECONDS\""
+    fi
   done <<< "$(head -n $[count+1] /datasets/ldbc-snb/${dataset}/substitution_parameters/interactive_${number}_param.txt | tail -n $count)"
 }
 
@@ -46,17 +52,32 @@ dataset="ldbc_snb_sf0100"
 #done
 
 # For TorcDB
-execQuery "7" "${query_count}" "100" "${dataset}"
-execQuery "8" "${query_count}" "100" "${dataset}"
-execQuery "4" "${query_count}" "100" "${dataset}"
-execQuery "11" "${query_count}" "100" "${dataset}"
-execQuery "1" "${query_count}" "100" "${dataset}"
-execQuery "2" "${query_count}" "100" "${dataset}"
-execQuery "12" "${query_count}" "100" "${dataset}"
-execQuery "10" "${query_count}" "100" "${dataset}"
-execQuery "13" "${query_count}" "25" "${dataset}"
-execQuery "14" "${query_count}" "25" "${dataset}"
-execQuery "6" "${query_count}" "10" "${dataset}"
-execQuery "5" "${query_count}" "5" "${dataset}"
-execQuery "9" "${query_count}" "5" "${dataset}"
-execQuery "3" "${query_count}" "5" "${dataset}"
+#execQuery "7" "${query_count}" "100" "${dataset}"
+#execQuery "8" "${query_count}" "100" "${dataset}"
+#execQuery "4" "${query_count}" "100" "${dataset}"
+#execQuery "11" "${query_count}" "100" "${dataset}"
+#execQuery "1" "${query_count}" "100" "${dataset}"
+#execQuery "2" "${query_count}" "100" "${dataset}"
+#execQuery "12" "${query_count}" "100" "${dataset}"
+#execQuery "10" "${query_count}" "100" "${dataset}"
+#execQuery "13" "${query_count}" "25" "${dataset}"
+#execQuery "14" "${query_count}" "25" "${dataset}"
+#execQuery "6" "${query_count}" "10" "${dataset}"
+#execQuery "5" "${query_count}" "5" "${dataset}"
+#execQuery "9" "${query_count}" "5" "${dataset}"
+#execQuery "3" "${query_count}" "5" "${dataset}"
+
+execQuery "7" "4486" "10" "${dataset}"
+execQuery "8" "4486" "10" "${dataset}"
+execQuery "4" "4486" "10" "${dataset}"
+execQuery "11" "4486" "10" "${dataset}"
+execQuery "1" "4486" "10" "${dataset}"
+execQuery "2" "4486" "10" "${dataset}"
+execQuery "12" "4486" "10" "${dataset}"
+execQuery "10" "4486" "10" "${dataset}"
+execQuery "13" "4486" "1" "${dataset}"
+execQuery "14" "4486" "1" "${dataset}"
+execQuery "6" "4486" "1" "${dataset}"
+execQuery "5" "200" "1" "${dataset}"
+execQuery "9" "200" "1" "${dataset}"
+execQuery "3" "200" "1" "${dataset}"
