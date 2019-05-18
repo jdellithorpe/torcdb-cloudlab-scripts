0438512505a9961bef0f3626c5f1b88b6b33c2ec
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-05-17 23:34:28 -0600

    Added parse-query-tester-output-50th.py

---
+++ parse-query-tester-output-50th.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+data = {}
+with open(sys.argv[1], "r") as f:
+  queryname = ""
+  queryargs = ""
+  querystat = {}
+  for line in f:
+    match = re.match("^cmd=\[(query[0-9]*) (.*)\]", line)
+    if match:
+      if bool(querystat):
+        if queryname in data:
+          data[queryname].append(querystat)
+        else:
+          data[queryname] = [querystat]
+        querystat = {}
+
+      queryname = match.group(1)
+      queryargs = match.group(2)
+      querystat["args"] = queryargs
+
+    match = re.match("^\s*([0-9]*)th\s+Percentile:\s+([0-9]*)", line)
+    if match:
+      querystat[match.group(1)] = match.group(2)
+    
+    match = re.match("\s*(Min|Max|Mean|Count):\s+([0-9]*)", line)
+    if match:
+      querystat[match.group(1)] = match.group(2)
+
+#print data
+for queryname in data:
+  print queryname,
+
+  with open(queryname + "-50th.csv", "w") as f:
+    for querystats in data[queryname]:
+      print querystats["50"],
+      f.write(querystats["50"] + "\n")
+
+    print
