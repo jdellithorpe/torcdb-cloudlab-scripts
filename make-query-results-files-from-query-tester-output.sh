2352a07384c26f61663cf539f39dcec0712a7f46
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-06-14 14:53:23 -0600

    Updated make-query-results-files-from-query-tester-output.sh

--- make-query-results-files-from-query-tester-output.sh
+++ make-query-results-files-from-query-tester-output.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-for i in {1..14}; do cat query_results_gathering_script_sf0001.out | grep "^LdbcQuery${i}Result{.*}\|cmd=\[query${i} " > query${i}_ldbc_snb_sf0001_results.txt; done
-for i in {1..14}; do sed -i 's/ --timeUnits=MICROSECONDS//' query${i}_ldbc_snb_sf0001_results.txt; done
+if [[ $# != 1 ]]
+then
+  echo "Parse the output of QueryTester to produce query\${i}_results.txt for"
+  echo "i in {1..14}"
+  echo ""
+  echo "Usage: make-query-results-files-from-query-tester-output.sh FILE"
+  echo "  FILE             QueryTester output to parse."
+
+  exit
+fi
+
+INPUT_FILE=$1
+
+for i in {1..14}; do cat ${INPUT_FILE} | grep "^LdbcQuery${i}Result{.*}\|^LdbcQuery${i}{.*}" > query${i}_results.txt; done
