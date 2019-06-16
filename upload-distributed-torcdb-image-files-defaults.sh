428f9c57d1bb05706dca30324d345f42e8763ae9
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-06-15 23:15:18 -0600

    Updated scripts.

--- upload-distributed-torcdb-image-files-defaults.sh
+++ upload-distributed-torcdb-image-files-defaults.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 #./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_interactive_validation/neo4j/torcdb2_df2_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
-#./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0001/torcdb2_df2_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
+#./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_interactive_validation/neo4j/torcdb2_df3_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
 #./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0001/torcdb_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
+#./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0001/torcdb2_df2_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
+#./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0001/torcdb2_df3_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
 #./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0010/torcdb2_df2_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
+#./upload-distributed-torcdb-image-files.sh 4servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0010/torcdb2_df3_8kbedgelists_ramcloud_image_files/tablets-4/ $(rccoordlocator) ~/RAMCloudUtils/
 #./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb_ramcloud_image_files/tablets-8/ $(rccoordlocator) ~/RAMCloudUtils/
 #./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb2_df1_ramcloud_image_files/tablets-8/ $(rccoordlocator) ~/RAMCloudUtils/
 #./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb2_df2_ramcloud_image_files/tablets-8/ $(rccoordlocator) ~/RAMCloudUtils/
-./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb2_df2_8kbedgelists_ramcloud_image_files/tablets-8/ $(rccoordlocator) ~/RAMCloudUtils/
+#./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb2_df2_8kbedgelists_ramcloud_image_files/tablets-8/ $(rccoordlocator) ~/RAMCloudUtils/
+./upload-distributed-torcdb-image-files.sh 8servers.txt /local/rcbackup/remote/ldbc-snb/ldbc_snb_sf0100/torcdb2_df3_8kbedgelists_ramcloud_image_files/tablets-8/ $(rccoordlocator) ~/RAMCloudUtils/
