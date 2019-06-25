29878c288e8eea42026431667d40ab91eefdd631
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-06-25 13:28:45 -0600

    Updated parse-query-tester-output-50th.py

--- parse-query-tester-output-50th.py
+++ parse-query-tester-output-50th.py
@@ -22,9 +22,22 @@ with open(sys.argv[1], "r") as f:
         queryname = match.group(1) + match.group(2)
       else:
         queryname = match.group(2)
-      
+
       queryargs = match.group(3)
       querystat["args"] = queryargs
+    else:
+      match = re.match("^cmd=\[(update[0-9]*) (.*)\]", line)
+      if match:
+        if bool(querystat):
+          if queryname in data:
+            data[queryname].append(querystat)
+          else:
+            data[queryname] = [querystat]
+          querystat = {}
+
+        queryname = match.group(1)
+        queryargs = match.group(2)
+        querystat["args"] = queryargs
 
     match = re.match("^\s*([0-9]*)th\s+Percentile:\s+([0-9]*)", line)
     if match:
